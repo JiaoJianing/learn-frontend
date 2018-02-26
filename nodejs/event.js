801a48601fa7f96c49e9ef9086eadffbba7882a8
jiaojn <jiaojn@skong.com>
2018-02-26 10:49:33 +0800

    nodejs-简单示例

---
+++ nodejs/event.js
@@ -0,0 +1,29 @@
+//引入events模块
+var events = require('events');
+//创建eventEmitter对象
+var eventEmitter = new events.EventEmitter();
+
+//创建事件处理程序
+var connectHandler = function connected(){
+	console.log('连接成功。');
+	
+	//触发 data_received事件
+	eventEmitter.emit('data_received');
+}
+
+//绑定connection事件处理程序
+eventEmitter.on('connection', connectHandler);
+
+//匿名函数绑定data_received事件
+eventEmitter.on('data_received', function(){
+	var i = 0;
+	while(i<10){
+		console.log('数据接收成功。');
+		i++;
+	}
+})
+
+//触发connection事件
+eventEmitter.emit('connection');
+
+console.log('程序执行完毕。');
